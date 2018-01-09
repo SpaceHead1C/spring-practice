6a2e2765422c431b5b509899ccf6c889c0327ce5
MSI\Andy <damh17@gmail.com>
2018-01-10 01:42:02 +0300

    Add controller class

---
+++ springboot/src/main/java/ru/nd/web/StackoverflowController.java
@@ -0,0 +1,22 @@
+package ru.nd.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.nd.model.StackoverflowWebsite;
+import ru.nd.service.StackoverflowService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stackoverflow")
+public class StackoverflowController {
+    @Autowired
+    private StackoverflowService stackoverflowService;
+
+    @RequestMapping
+    //@ResponseBody cuz have @RestController
+    public List<StackoverflowWebsite> getListOfProviders() {
+        return stackoverflowService.findAll();
+    }
+}
