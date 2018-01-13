3d2d745ca57dc1deaebadf0d0a76d8310ddbe54a
MSI\Andy <damh17@gmail.com>
2018-01-13 22:54:04 +0300

    Use Lombok (with install plugin to IDEA)

--- springboot/src/main/java/ru/nd/model/StackoverflowWebsite.java
+++ springboot/src/main/java/ru/nd/model/StackoverflowWebsite.java
@@ -1,38 +1,20 @@
 package ru.nd.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data // activate Lombok and generate default constructor by default, getters and setters
+@AllArgsConstructor // create constructor with all arguments only if not set @NoArgsConstructor
+@NoArgsConstructor // create default constructor anyway
+@Document // for MongoDB
 public class StackoverflowWebsite {
-    private final String id;
-    private final String website;
-    private final String iconImageUrl;
-    private final String title;
-    private final String description;
-
-    public StackoverflowWebsite(String id, String website, String iconImageUrl, String title, String description) {
-        this.id = id;
-        this.website = website;
-        this.iconImageUrl = iconImageUrl;
-        this.title = title;
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public String getIconImageUrl() {
-        return iconImageUrl;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    @Id
+    private String id;
+    private String website;
+    private String iconImageUrl;
+    private String title;
+    private String description;
 }
