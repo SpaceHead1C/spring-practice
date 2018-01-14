75db2d17e4af8409891d185c275ac62e536444a2
MSI\Andy <damh17@gmail.com>
2018-01-14 20:52:25 +0300

    Add logic to Application class

--- goeuro-test-assignment/src/main/java/com/goeuro/testassignment/Application.java
+++ goeuro-test-assignment/src/main/java/com/goeuro/testassignment/Application.java
@@ -1,16 +1,41 @@
 package com.goeuro.testassignment;
 
+import com.goeuro.testassignment.service.CsvSuggestionConverter;
+import com.goeuro.testassignment.service.CsvSuggestionWriter;
+import com.goeuro.testassignment.service.GoEuroApiClient;
 import com.google.common.collect.ImmutableList;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+    @Autowired
+    private CsvSuggestionWriter csvSuggestionWriter;
+    @Autowired
+    private GoEuroApiClient goEuroApiClient;
+    @Autowired
+    private CsvSuggestionConverter csvSuggestionConverter;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build()));
+    }
+
     public static void main(String[] args) {
-        // if have class wich implements CommandLineRunner the Spring run method "run" automaticly
+        new SpringApplicationBuilder(Application.class)
+                .bannerMode(Banner.Mode.OFF) // without Spring Boot banner
+                .run(args);
+        // if have class wich implements CommandLineRunner the Spring see method "run"
     }
 
     @Override
@@ -18,7 +43,7 @@ public class Application implements CommandLineRunner {
         String cityName = args[0].trim();
         String filename = cityName + ".csv";
 
-        csvSuggestionWriter.write(filename, goEuroApiClient.findSuggestionsByCity().stream()
+        csvSuggestionWriter.write(filename, goEuroApiClient.findSuggestionsByCity(cityName).stream()
                 .map(csvSuggestionConverter::toCsvSuggestionDto)
                 .collect(collectingAndThen(toList(), ImmutableList::copyOf)));
     }
