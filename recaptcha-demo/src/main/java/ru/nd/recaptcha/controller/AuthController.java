cccc2012406d1592d93bdc5b81153f0759211cf1
MSI\Andy <damh17@gmail.com>
2018-01-15 23:27:46 +0300

    Add empty controller class

---
+++ recaptcha-demo/src/main/java/ru/nd/recaptcha/controller/AuthController.java
@@ -0,0 +1,16 @@
+package ru.nd.recaptcha.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+//    @ResponseStatus(HttpStatus.OK)
+//    @RequestMapping(value = "/register", method = RequestMethod.POST)
+//    public void register(RegisterDto registerDto) {
+//        //
+//    }
+}
