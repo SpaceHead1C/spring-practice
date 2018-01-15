ed309a8db0f9f89d74c37523e5b022f02803229b
MSI\Andy <damh17@gmail.com>
2018-01-15 23:51:47 +0300

    Add object for authorisation Show allert for debug

--- recaptcha-demo/src/main/resources/public/js/main.js
+++ recaptcha-demo/src/main/resources/public/js/main.js
@@ -1,3 +1,7 @@
 angular.module("reCaptchaDemo", [])
     .controller("AppCtrl", function ($scope){
+        $scope.auth = {};
+        $scope.sendForm = function(auth) {
+            window.alert(auth.email);
+        }
     });
